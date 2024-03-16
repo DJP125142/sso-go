4619d347343268fff128dd7e649dd5bc1e18cb76
jasper <929435979@qq.com>
2024-03-17 03:15:06 +0800

    cors allow cookie

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -8,6 +8,8 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")       // 允许携带Cookie
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Authorization") // 暴露其他头部
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
